7cc455df2dc064bbd3ab8323a007559b4e8dc26e
Vikas Yadav <geekyvikas240052@gmail.com>
2024-05-21 00:12:42 +0530

    feat(web): add next theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
+"use client";
+// It must be not a client component we have to move client components into separate file
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { useTheme } from "next-themes";
 
 export default function Home() {
+  const { theme, setTheme } = useTheme();
+
+  const updateTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Shadcn Button</Button>
+    <main className="flex min-h-screen flex-col items-center bg-orange-50 dark:bg-gray-950 justify-center">
+      <Button onClick={updateTheme}>Theme Toggle</Button>
     </main>
   );
 }
